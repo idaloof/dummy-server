e1ffe19f768614e9a9527b117cae9b224b2c4f10
Martin Fooladi <martin.fooladi@gmail.com>
2023-11-12 18:46:59 +0100

    Added middleware for admin route /register

---
+++ middleware/admin-auth.js
@@ -0,0 +1,51 @@
+/**
+ * @description Middleware functions to check if admin token is provided
+ */
+
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+
+/**
+ * @description Function to validate admin token
+ *
+ * @param {String} token Admin token
+ * 
+ * @returns {Boolean}
+ */
+function verfiyToken(token) {
+    if (token) {
+        try {
+            jwt.verify(token, jwtSecret);
+            return true;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * @description Function to check if admin token is provided
+ *
+ * @param {Request} req Request object
+ * @param {Response} res Response object
+ * @param {Function} next Next function
+ *
+ * @returns {void}
+ */
+function adminAuthMiddleware(req, res, next) {
+    const token = req.headers['admin-token'];
+
+    if (!token) {
+        return res.status(401).send('Admin token is required');
+    }
+
+    if (!verfiyToken(token)) {
+        return res.status(403).send('Invalid admin token');
+    }
+
+    next();
+}
+
+module.exports = adminAuthMiddleware
