a6f3b87c6b4dc7ad640001c0b2a0a9b0f0489765
Martin Fooladi <martin.fooladi@gmail.com>
2023-11-12 19:00:16 +0100

    Making sure admin routes are handled correctly

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -6,12 +6,12 @@ const adminAuthMiddleware = require('../../middleware/admin-auth.js')
  * @description Collection of REST API endpoints
  */
 
+router.use('/admin', adminAuthMiddleware, require('./admin.js'))
 router.use('/', require('./bikes.js'))
 router.use('/', require('./cities.js'))
 router.use('/', require('./login.js'))
 router.use('/', require('./trips.js'))
 router.use('/', require('./users.js'))
 router.use('/', require('./zones.js'))
-router.use('/', adminAuthMiddleware, require('./admin.js'))
 
 module.exports = router
