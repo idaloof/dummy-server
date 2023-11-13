1aa4901e946fb438737af19da30a264992c02b7f
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-13 23:48:35 +0100

    Remove unused next function from parameter list

--- middleware/errors.js
+++ middleware/errors.js
@@ -3,9 +3,8 @@
  * @param {Error} err
  * @param {import("express").Request} req
  * @param {import("express").Response} res
- * @param {import("express").NextFunction} next
  */
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res) {
     console.error(err);
     res.status(500).send("Something broke!");
 }
