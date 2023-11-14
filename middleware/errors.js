8988cef51520fabdf6a2dd862b53b42c15ca2c9b
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-14 08:56:39 +0100

    Include next function

--- middleware/errors.js
+++ middleware/errors.js
@@ -3,10 +3,14 @@
  * @param {Error} err
  * @param {import("express").Request} req
  * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
  */
-function errorHandler(err, req, res) {
+// eslint-disable-next-line no-unused-vars
+function errorHandler(err, req, res, next) {
     console.error(err);
-    res.status(500).send("Something broke!");
+    res.status(500).json({
+        errors: "Something broke!"
+    });
 }
 
 export default errorHandler;
